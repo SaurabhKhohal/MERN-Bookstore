80d7339775fe522137f1b00ca30e275698ad7fc1
Saurabh Khohal <saurabhkhohal2001@gmail.com>
2024-01-20 14:32:34 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 3002;
+
+export const mongoD = 'mongodb+srv://skkp:skkp@cluster0.m449cq0.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
